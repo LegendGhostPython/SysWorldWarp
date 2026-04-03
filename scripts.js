53137555131f231417bf85f17133b284c09a5136
LegendGhostPython <legendghostpython@gmail.com>
2026-04-03 13:47:44 -0300

    update scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//Redimensionar imagem
+//img1 = document.querySelector(".img1");
+//img1.style.height = "700px";
+//img1.style.width= "300px";
+//img1.style.position="relative";
+//img1.style.left = "0px";
+//img2 = document.querySelector(".img2");
+//img2.style.height="700px";
+//img2.style.width="300px";

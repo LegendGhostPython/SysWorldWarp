6440bb768d95aebb31575cc650f8a6a57ac3875b
LegendGhostPython <legendghostpython@gmail.com>
2026-04-03 13:42:50 -0300

    update Index.php

---
+++ Index.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <meta Lang="pt_BR">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+    <body>
+     <header>
+         <img class="logo" src="src/Sys.jpg" alt="#" title="Logo" onfocus="#"/>
+     </header>   
+     <nav>
+         <table class="Menu1">
+             <td onclick="#">
+            test
+            </td>
+            <td>
+                test2
+            </td>
+              <td> test 1.1</td>
+             <td> test 2.2</td>
+            
+         </table>
+     </nav>
+     <main>
+         <h1>Faça seu login</h1>
+         <form action="Sobre/Index.php" method="get">
+         <section>
+             <label for="User" >User:</label>
+             <input type="text" name="txtform">
+        </section>
+        <Br>
+        <section>
+            <label  for="Password">Senha:</label>
+             <input type="password" name="cadpass">
+            <button type="submit">enviar</button>
+         </section>
+         </form>
+         <hr>
+         <div class="imagens flex-img">
+             <img  class="img1" src="src/img1.jpg" height="1px" width="1px">
+             <img class="img2" src="src/img2.jpg"
+             height="1px" width="1px">
+             <img class="img3" src="src/img3.jpg" height="1px" width="1px">
+             <img class="img4"src="src/img4.jpg" height="1px" width="1px">
+    
+         </div>
+         <hr>
+     </main>
+    <script src="scripts.js">
+    </script>
+    </body>
+</html>
